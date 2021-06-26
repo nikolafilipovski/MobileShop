40e4f76b5c8d6a7aa0ec0a4fcd42243210b32908
Nikola Filipovski <nfilipovski@yahoo.com>
2021-06-27 01:17:59 +0200

    27.06

--- MobileShopApp/MobileShop/Program.cs
+++ MobileShopApp/MobileShop/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using NLog.Web;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,30 +21,27 @@ namespace MobileShop
                 logger.Debug("init main");
                 CreateHostBuilder(args).Build().Run();
             }
-            catch (Exception exception)
+            catch (Exception ex)
             {
-                //NLog: catch setup errors
-                logger.Error(exception, "Stopped program because of exception");
+                logger.Error(ex, "Stopped program because of exception");
                 throw;
             }
             finally
             {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
                 NLog.LogManager.Shutdown();
             }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args)
-      .ConfigureWebHostDefaults(webBuilder =>
-      {
-          webBuilder.UseStartup<Startup>();
-      })
-      .ConfigureLogging(logging =>
-      {
-          logging.ClearProviders();
-          logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-      })
-      .UseNLog();  // NLog: Setup NLog for Dependency injection
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>()
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.SetMinimumLevel(LogLevel.Trace);
+                    })
+                    .UseNLog();  
+                });
     }
 }
---
+++ MobileShopApp/MobileShop/wwwroot/js/form-validation.js
@@ -0,0 +1,65 @@
+﻿const jqueryValidate = require("../jquery-validate/jquery.validate");
+
+$(function () {
+    $("form[name='phoneCreate']").validate({
+        success: "valid",
+        onkeyup: true, // validation as we type the data
+        focusInvalid: true, // field when invalid
+    }),
+
+    rules: {
+        Model: {
+            required: true,
+            minlength: 6
+        },
+        Price: {
+            required: true,
+            minlength: 3
+        },
+        Display: {
+            required: true,
+            minlength: 6
+        },
+        Memory: {
+            required: true,
+            minlength: 6
+        },
+        Battery: {
+            required: true,
+            minlength: 6
+        },
+        Manufacturer: {
+            required: true
+        }
+    },
+
+    messages: {
+        Model: {
+            required: "Phone model is required!",
+            minlength: jqueryValidate.format("At least {0} characters are required!")
+        },
+        Price: {
+            required: "Phone price is required!",
+            minlength: jqueryValidate.format("At least {0} characters are required!")
+        },
+        Display: {
+            required: "Display type is required!",
+            minlength: jqueryValidate.format("At least {0} characters are required!")
+        },
+        Memory: {
+            required: "Phone memory is required!",
+            minlength: jqueryValidate.format("At least {0} characters are required!")
+        },
+        Battery: {
+            required: "Phone battery is required!",
+            minlength: jqueryValidate.format("At least {0} characters are required!")
+        },
+        Manufacturer: {
+            required: "Please select a manufacturer!"
+        }
+    }
+    submitHandler: function (form) {
+        form.submit();
+    }
+});
+
